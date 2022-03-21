c3a46d0aa728830c722addfcb788ddfca76d512b
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-22 09:32:23 +1300

    add: player layout

---
+++ components/playerLayout.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/layout"
+import SideBar from "./sideBar"
+
+export default function PlayerLayout({ children }) {
+    return (
+        <Box>
+            Layout
+            {children}
+        </Box>
+    )
+}
