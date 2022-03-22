7c3ca53a28cafa07e96ffb356b30e8651bc8b693
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-23 09:50:35 +1300

    change: define component sizing values in PlayerLayout for better centralised control
    Originally the design is to leave sizing values inside of each individual component for better encapsulation. But this approach turns out to be cumbersome, therefore a more centralised approach is chosen.

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/layout";
+import { calc } from "@chakra-ui/react";
 import SideBar from "./sideBar";
 
 export default function PlayerLayout({ children }) {
   const sideBarWidth = "15rem";
   const playerHeight = "5rem";
   return (
+    // In terms of layout, my approach here may be less orthodox - in stead of putting the sizing values (namely sideBarWidth, playerHeight etc.) inside of each made-from-scratch component, I extract them and let them sit in the PlayerLayout component layer so that the control is centralised. If these components are to be reused in future, default sizing values should be put inside of the components for better encapsulation.
     <Box width="100vw" height="100vh" border="1px solid lightgray">
       <Box
         id="sideBarContainer"
@@ -12,7 +14,8 @@ export default function PlayerLayout({ children }) {
         top="0"
         left="0"
         width={sideBarWidth}
-        border="1px solid yellow"
+        height={`calc(100vh - ${playerHeight})`}
+        // border="1px solid yellow"
       >
         <SideBar />
       </Box>
@@ -29,6 +32,9 @@ export default function PlayerLayout({ children }) {
         position="absolute"
         left="0"
         bottom="0"
+        width="100vw"
+        height={playerHeight}
+        bg="gray.700"
         border="1px solid blue"
       >
         Player
