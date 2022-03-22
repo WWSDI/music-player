6974f6659c6213feffb71e1b711437c8ab34af99
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-23 09:57:33 +1300

    add: music player logo

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,10 +1,40 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, List, ListIcon, ListItem } from "@chakra-ui/layout";
+import NextImage from "next/image";
+import { MdHome, MdLibraryMusic, MdSearch } from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/"
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search"
+  },
+  {
+    name: "My Library",
+    icon: MdLibraryMusic,
+    route:"/library"
+  },
+];
 
 export default function SideBar() {
   return (
-    <Box>
-      SideBar
-      <Box></Box>
+    <Box bg="black" color="white" width="100%" height="100%" padding="1rem">
+      <Box>
+        {/* All static files are stored in public folder, so that when you use "/" as base URL to refer to any files in the public folder */}
+        <NextImage src="/logo.svg" width={100} height={50} />
+      </Box>
+      <List spacing={3}>
+        {navMenu.map((item) => (
+          <ListItem>
+            <ListIcon as={item.icon} color='white.100'/>
+            {item.name}
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 }
